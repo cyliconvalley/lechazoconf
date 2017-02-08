42221264922cbb14fcbeae4e38f19ead73e902e4
Javier Gamarra <nhpatt@gmail.com>
2017-02-08 12:27:03 +0100

    I’m dyslexic with dates

--- js/custom.js
+++ js/custom.js
@@ -57,7 +57,7 @@ jQuery(document).ready(function(){
         var $tz_meetup_countdown = jQuery('.tz_meetup_countdown');
         if ( $tz_meetup_countdown.length > 0 ) {
 
-            var $data_countdown     =   '2017/05/20 09:00:00';
+            var $data_countdown     =   '2017/05/27 09:00:00';
             var $data_text_ended    =   'La LechazoConf ya ha acabado, nos vemos en la próxima!';
 
             var $data_text_day      =   'días';
